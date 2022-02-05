9df5151e0062e44479f71aa0eb7bbf0aaf1d361b
karthikbenki <karthikmc007@gmail.com>
2022-02-05 18:18:22 +0530

    Added: Repeat Roll a dice logic till position 100 reaches

--- src/com/snakeladder/SnakeAndLadderComputation.java
+++ src/com/snakeladder/SnakeAndLadderComputation.java
@@ -9,25 +9,32 @@ public class SnakeAndLadderComputation {
 
 	public static void main(String[] args) {
 		int position = 0;
-		Random ran = new Random();
-		int dice = ran.nextInt(6)+1;
-		System.out.println("dice: "+dice);
-		int optionCheck = ran.nextInt(3);
-		System.out.println("optionCheck: "+optionCheck);
-		
-		if (optionCheck == LADDER) {
-			System.out.println("ladder!!!");
-			position = position + dice;
-		}
-		else if (optionCheck == SNAKE && position > dice) {
-			System.out.println("snake!!!");
-			position = position - dice;
-		}
-		else {
-			System.out.println("noPlay!!!");
-			position = position;
-		}
 		System.out.println("position: "+position);
+		while (position<100) {
+			Random ran = new Random();
+			int dice = ran.nextInt(6)+1;
+			System.out.println("dice: "+dice);
+			int optionCheck = ran.nextInt(3);
+			System.out.println("optionCheck: "+optionCheck);
+			
+			
+			if (optionCheck == LADDER) {
+				System.out.println("ladder!!!");
+				position = position + dice;
+			}
+			else if (optionCheck == SNAKE) {
+				System.out.println("snake!!!");
+				position = position - dice;
+			}
+			else {
+				System.out.println("noPlay!!!");
+				position = position;
+			}
+			if (position<0) {
+				position =0;
+			}
+			System.out.println("position: "+position);
+		}
 	}
 
 }
