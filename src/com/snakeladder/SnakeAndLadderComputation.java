49ce2e5c04082005d2dd40db5e72e9c49335e3f7
karthikbenki <karthikmc007@gmail.com>
2022-02-05 17:28:35 +0530

    Added: Rolling a die to get 1 to 6 logic

--- src/com/snakeladder/SnakeAndLadderComputation.java
+++ src/com/snakeladder/SnakeAndLadderComputation.java
@@ -1,10 +1,14 @@
 package com.snakeladder;
 
+import java.util.Random;
+
 public class SnakeAndLadderComputation {
 
 	public static void main(String[] args) {
 		int postion = 0;
-		
+		Random ran = new Random();
+		int dice = ran.nextInt(6)+1;
+		System.out.println("dice: "+dice);
 	}
 
 }
